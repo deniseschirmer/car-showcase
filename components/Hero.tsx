01598e9f89b09536e01798e884b700f5cf149687
Denise Schirmer <denise_liima@hotmail.com>
2023-07-16 09:52:27 -0300

    feat: Starting project and deploying hero

---
+++ components/Hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Encontre, reserve, alugue um carro - rápido e super fácil!
+        </h1>
+
+        <p className="hero__subtitle">
+          Simplifique sua experiência de aluguel de carros com nossa reserva sem
+          esforço no processo.
+        </p>
+
+        <CustomButton
+          title="Explorar carros"
+          containerStyles="bg-primary-blue
+        text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
