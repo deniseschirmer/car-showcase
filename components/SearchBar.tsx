261ea68000c9e48e1ead374a01cbc872b9545859
Denise Schirmer <denise_liima@hotmail.com>
2023-07-23 09:36:18 -0300

    Feat: SearchManuFacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
